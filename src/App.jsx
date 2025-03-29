57e9ea1b22b69d5bc63defdd588b487486060fa8
ShehzadIqbal1 <shehzad.iqbal622@gmail.com>
2025-03-29 08:44:45 +0500

    small projects

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+// Message selector app 
+
+// useState messages app
+// import { useState } from "react";
+// import "./App.css";
+// const messages = [
+//   "Learn React ⚛️",
+//   "Apply for jobs 💼",
+//   "Invest new income 🤑",
+// ];
+// function App() {
+//   const [step, setStep] = useState(1);
+//   const [isOpen, setIsOpen] = useState(true);
+
+
+//   function handlePrevious() {
+//     if (step > 1) setStep((s)=> s - 1);
+//   }
+
+//   function handleNext() {
+//     if (step < 3) setStep((s)=> s + 1);
+//   }
+//   return (
+//     <>
+//       <button className="close" onClick={() => setIsOpen(!isOpen)}>
+//         {" "}
+//         &times;
+//       </button>
+//       {isOpen && (
+//         <div className="steps">
+//           <div className="numbers">
+//             <div className={step === 1 ? "active" : ""}>1</div>
+//             <div className={step === 2 ? "active" : ""}>2</div>
+//             <div className={step === 3 ? "active" : ""}>3</div>
+//           </div>
+
+//           <p className="message">
+//             Step {step}:{messages[step - 1]}
+//           </p>
+
+//           <div className="buttons">
+//             <button onClick={handlePrevious}>Previous</button>
+//             <button onClick={handleNext}>Next</button>
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+//Week days counter app
+
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep]= useState(0);
+  const date = new Date("2025/03/28");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+        <div>
+        <button onClick={() => setStep((step) => step - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((step) => step + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((count) => count - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((count) => count + step)}>+</button>
+      </div>
+
+      <p>
+        <span> {count === 0 ? "Today is " : count > 0 ? `${count} days from today is `: `${Math.abs(count)} days ago was `} </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
+
+export default App;
