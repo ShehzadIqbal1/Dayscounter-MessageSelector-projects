0f4c8fd67ad813362dd320054243b2958357f046
ShehzadIqbal1 <shehzad.iqbal622@gmail.com>
2025-06-28 20:22:01 +0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// Message selector app 
+// Message selector app
 
 // useState messages app
 // import { useState } from "react";
@@ -11,7 +11,6 @@
 // function App() {
 //   const [step, setStep] = useState(1);
 //   const [isOpen, setIsOpen] = useState(true);
-
 
 //   function handlePrevious() {
 //     if (step > 1) setStep((s)=> s - 1);
@@ -50,8 +49,6 @@
 
 // export default App;
 
-
-
 //Week days counter app
 
 import { useState } from "react";
@@ -66,13 +63,31 @@ function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const [step, setStep]= useState(0);
+  const [step, setStep] = useState(1);
   const date = new Date("2025/03/28");
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+  console.log(count, step)
+
   return (
     <div>
-        <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setStep((step) => step - 1)}>-</button>
         <span>Step: {step}</span>
         <button onClick={() => setStep((step) => step + 1)}>+</button>
@@ -85,9 +100,20 @@ function Counter() {
       </div>
 
       <p>
-        <span> {count === 0 ? "Today is " : count > 0 ? `${count} days from today is `: `${Math.abs(count)} days ago was `} </span>
+        <span>
+          {" "}
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}{" "}
+        </span>
         <span>{date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1  ? (
+        <div>
+      <button onClick={handleReset}>Reset</button>
+      </div>): null}
     </div>
   );
 }
